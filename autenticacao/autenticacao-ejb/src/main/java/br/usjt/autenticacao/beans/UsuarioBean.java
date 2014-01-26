ed4ecd14a660b6558991ab6ba82839e8ae2621a4
Thiago Souza Cardoso <kamui.thiago@gmail.com>
2014-01-26 21:18:54 -0200

    modificado ear

--- autenticacao/autenticacao-ejb/src/main/java/br/usjt/autenticacao/beans/UsuarioBean.java
+++ autenticacao/autenticacao-ejb/src/main/java/br/usjt/autenticacao/beans/UsuarioBean.java
@@ -9,46 +9,53 @@ import javax.ejb.Stateless;
 import br.usjt.autenticacao.facade.UsuarioBeanRemote;
 import br.usjt.autenticacao.model.Usuario;
 
-
 @Stateless
 @LocalBean
 @Remote(UsuarioBeanRemote.class)
 public class UsuarioBean implements UsuarioBeanRemote {
 
-	
-	
     public UsuarioBean() {
     }
-    
 
-	@Override
-	public void salvar(Usuario usuario) {
-		
-	}
+    @Override
+    public void salvar(Usuario usuario) {
 
-	@Override
-	public void atualizar(Usuario usuario) {
-		
-	}
+    }
 
-	@Override
-	public void deletar(Usuario usuario) {
-		
-	}
+    @Override
+    public void atualizar(Usuario usuario) {
 
-	@Override
-	public Usuario consultarPorCPF(String cpf) {
-		return null;
-	}
+    }
 
-	@Override
-	public Usuario consultarPorEmail(String email) {
-		return null;
-	}
+    @Override
+    public void deletar(Usuario usuario) {
 
-	@Override
-	public List<Usuario> consultarTodosUsuarios() {
-		return null;
-	}
-    
+    }
+
+    @Override
+    public Usuario consultarPorCPF(String cpf) {
+        return null;
+    }
+
+    @Override
+    public Usuario consultarPorEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Usuario> consultarTodosUsuarios() {
+
+        Usuario u = new Usuario();
+        u.setNome("Thiago");
+        u.setSobrenome("Thiago");
+
+        Usuario u2 = new Usuario();
+        u2.setNome("Thiago");
+        u2.setSobrenome("Thiago");
+
+        System.out.println(u.equals(u2));
+
+        return null;
+    }
+
 }
--- autenticacao/autenticacao-web/src/main/java/br/usjt/autenticacao/mbeans/LoginController.java
+++ autenticacao/autenticacao-web/src/main/java/br/usjt/autenticacao/mbeans/LoginController.java
@@ -1,13 +1,22 @@
 package br.usjt.autenticacao.mbeans;
 
+import javax.ejb.EJB;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 
+import br.usjt.autenticacao.facade.UsuarioBeanRemote;
 
 @Named
 @SessionScoped
 public class LoginController {
 
-	
-	
+    @EJB
+    UsuarioBeanRemote bean;
+
+    public void teste() {
+
+        bean.consultarTodosUsuarios();
+
+    }
+
 }
